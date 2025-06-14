9434ee48eae0ab8766a2f83d5fe85b835db9a14c
quduss <shuaib.qudus@yahoo.com>
2025-06-14 20:57:16 +0100

    Write function that splits the user's input into words based on whitespace

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	text = strings.TrimSpace(text)
+	text = strings.ToLower(text)
+	words := strings.Fields(text)
+	return words
+}
