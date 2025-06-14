41cb61e8a4ed959ed91397957aa83bb67f7f0d49
quduss <shuaib.qudus@yahoo.com>
2025-06-14 22:10:34 +0100

    Build the bones of the Pokedex REPL

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Printf("Pokedex > ")
+
+		if !scanner.Scan() {
+			// Handle EOF or error
+			break
+		}
+
+		input := scanner.Text()
+		words := cleanInput(input)
+
+		if len(words) > 0 {
+			fmt.Printf("Your command was: %s\n", words[0])
+		}
+	}
 }
 
 func cleanInput(text string) []string {
