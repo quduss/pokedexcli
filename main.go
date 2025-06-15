cf097b2a959d6b05764686f06a2e4c49314b6d42
quduss <shuaib.qudus@yahoo.com>
2025-06-15 22:18:08 +0100

    Print an empty line after Usage:

--- main.go
+++ main.go
@@ -33,6 +33,7 @@ func commandExit() error {
 func commandHelp() error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
+	fmt.Println()
 	for _, cmd := range commands {
 		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
