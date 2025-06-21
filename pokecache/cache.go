66b1f7f4292ebd2d00612534ec0bf655a14719df
quduss <shuaib.qudus@yahoo.com>
2025-06-21 17:14:35 +0100

    Create Cache struct

--- pokecache/cache.go
+++ pokecache/cache.go
@@ -1,8 +1,17 @@
 package pokecache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
+}
+
+type Cache struct {
+	mu       sync.Mutex
+	cache    map[string]cacheEntry
+	interval time.Duration
 }
